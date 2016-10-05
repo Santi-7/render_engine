4333f7fb15cf5ebbe0412611939ac073121548a0
Santi Gil <santi.gil.begue7@gmail.com>
2016-10-05 15:38:21 +0200

    Add sphere figure

---
+++ src/figures/sphere.cpp
@@ -0,0 +1,15 @@
+/* ---------------------------------------------------------------------------
+** sphere.cpp
+** TODO: Add doc.
+**
+** Author: Miguel Jorge Galindo Ramos, NIA: 679954
+**         Santiago Gil Begué, NIA: 683482
+** -------------------------------------------------------------------------*/
+
+#include <sphere.hpp>
+
+Sphere(const Point center, const float radius)
+{
+    mCenter = center;
+    mRadius = radius;
+}
