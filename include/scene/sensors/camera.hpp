6dddf89fed04e1ae6925276f2b1d5c9ca655c5f8
mjgal <mjgalindoramos@gmail.com>
2016-10-06 15:53:51 +0200

    Add basic Camera implementation

--- include/scene/sensors/camera.hpp
+++ include/scene/sensors/camera.hpp
@@ -9,8 +9,27 @@
 #ifndef RAY_TRACER_CAMERA_HPP
 #define RAY_TRACER_CAMERA_HPP
 
+#include <vect.hpp>
+#include <point.hpp>
+
 class Camera {
 
+public:
+    Camera(Vect x, Vect y, Vect z, Point focalPoint, float fieldOfVision, float imagePlaneDistance);
+
+    Vect GetX();
+
+    Vect GetY();
+
+    Vect GetZ();
+
+private:
+
+    Point mFocalPoint;
+    Vect mX, mY, mZ;
+
+    float mFOV;
+    float mImagePlaneDistance;
 };
 
 #endif // RAY_TRACER_CAMERA_HPP
