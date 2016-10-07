14889636838f430949c7f0f826c219da3d2fcff8
mjgalindo <mjgalindoramos@gmail.com>
2016-10-07 10:37:12 +0200

    Add missing comma

--- src/scene/sensors/camera.cpp
+++ src/scene/sensors/camera.cpp
@@ -9,7 +9,7 @@
 #include <camera.hpp>
 
 Camera::Camera(const Vect &up, const Vect &right,
-               const Vect &towards,const Point &focalPoint,
+               const Vect &towards, const Point &focalPoint,
                const float fieldOfVision, const float imagePlaneDistance)
 :  mUp(up), mRight(right), mTowards(towards), mFocalPoint(focalPoint),
    mFOV(fieldOfVision), mViewPlaneDistance(imagePlaneDistance)
