aa5269f2acefba3412ccc59b75eb2bc9a7c927a7
Santi Gil <santi.gil.begue7@gmail.com>
2016-10-08 14:31:31 +0200

    Create a class with scene samples

---
+++ include/scene/sceneSamples.hpp
@@ -0,0 +1,17 @@
+/* ---------------------------------------------------------------------------
+** sceneSamples.hpp
+** TODO: Add doc.
+**
+** Author: Miguel Jorge Galindo Ramos, NIA: 679954
+**         Santiago Gil Begué, NIA: 683482
+** -------------------------------------------------------------------------*/
+
+#include <scene.hpp>
+
+// TODO: Add doc.
+/**
+ * .
+ *
+ * @return .
+ */
+Scene CornellBox();
--- src/scene/scene.cpp
+++ src/scene/scene.cpp
@@ -25,16 +25,20 @@ void Scene::AddShape(const Shape &shape)
 
 void Scene::Render() const
 {
+    // The current pixel. We begin with the first one (0,0).
     Point currentPixel = mCamera.GetFirstPixel();
+    // The first pixel of the current row.
     Point currentRow = currentPixel;
     // For all the pixels, trace a ray of light.
     for (unsigned int i = 0; i < mCamera.GetHeight(); ++i)
     {
         for (unsigned int j = 0; j < mCamera.GetWidth(); ++j)
         {
+            // Next pixel.
             currentPixel = currentPixel + mCamera.GetRight() * mCamera.GetPixelSize();
             // TODO: Trace the ray of light.
         }
+        // Next row.
         currentRow = currentRow - mCamera.GetUp() * mCamera.GetPixelSize();
         currentPixel = currentRow;
     }
