4161ea98dac74afd3128db38dd2cdc8e79e653fb
Santi Gil <santi.gil.begue7@gmail.com>
2016-10-11 10:13:57 +0200

    Remove dead code

--- src/containers/lightRay.cpp
+++ src/containers/lightRay.cpp
@@ -12,13 +12,6 @@ LightRay::LightRay(const Point &source, const Point &destination)
 : mSource(source), mDirection((destination - source).Normalise())
 {}
 
-LightRay::LightRay(const Point &source, const Point &destination,
-                   const float &distance)
-: mSource(source), mDirection((destination - source).Normalise())
-{
-    distance
-}
-
 Point LightRay::GetPoint(const float t) const
 {
     return mSource + mDirection * t;
