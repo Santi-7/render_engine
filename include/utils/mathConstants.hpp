70e5ae95796b730cde1895f9787a07f15ea14873
Santi Gil <santi.gil.begue7@gmail.com>
2016-11-24 20:48:10 +0100

    Add most usual refractive indexes

--- include/utils/mathConstants.hpp
+++ include/utils/mathConstants.hpp
@@ -9,6 +9,17 @@
 #ifndef RAY_TRACER_MATH_CONSTANTS_H
 #define RAY_TRACER_MATH_CONSTANTS_H
 
+typedef float refractiveIndex;
+
 static constexpr float PI = 3.1415926535897932f;
+
+// TODO: Add doc.
+/* . */
+static constexpr refractiveIndex VACUUM_RI  = 1.0f;
+static constexpr refractiveIndex AIR_RI     = 1.0002926f;
+static constexpr refractiveIndex WATER_RI   = 1.333f;
+static constexpr refractiveIndex QUARTZ_RI  = 1.544f;
+static constexpr refractiveIndex GLASS_RI   = 1.52f;
+static constexpr refractiveIndex DIAMOND_RI = 2.42f;
 
 #endif // RAY_TRACER_MATH_CONSTANTS_H
