96ad74a0d4f47fdad8442306234754022373c3cf
mjgalindo <mjgalindoramos@gmail.com>
2016-11-28 16:59:37 +0100

    Make lamberian gray so that it can be used in indirect light

--- include/scene/materials/material.hpp
+++ include/scene/materials/material.hpp
@@ -92,7 +92,7 @@ private:
 };
 
 static const shared_ptr<Material> MIRROR = make_shared<Material>(Material(BLACK, BLACK, 0, WHITE, BLACK));
-static const shared_ptr<Material> LAMBERTIAN = make_shared<Material>(Material(WHITE, BLACK, 0, BLACK, BLACK));
+static const shared_ptr<Material> LAMBERTIAN = make_shared<Material>(Material(GRAY, BLACK, 0, BLACK, BLACK));
 static const shared_ptr<Material> SPECKLED_LAMBERTIAN = make_shared<Material>(Material(GRAY, GRAY, 20, BLACK, BLACK));
 static const shared_ptr<Material> GLASS = make_shared<Material>(Material(BLACK,BLACK,0,BLACK,GRAY));
 
